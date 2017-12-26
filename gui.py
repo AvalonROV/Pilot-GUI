1ae5c92cfd82af14c60608d5908c9bc19b01d52d
Khaled <k.saad@ossila.com>
2017-12-26 15:59:52 +0000

    Added comments to the string_formatter function

--- gui.py
+++ gui.py
@@ -50,72 +50,71 @@ app = QApplication(sys.argv) # Creat a new QApplication object. This manages
 LED1 = 0
 LED2 = 0
 
+
 class Window(QWidget):
-    
+    """
+    This class is the base class of all user interface objects. 
+    """
     def __init__(self):
         super(Window, self).__init__()
         
         self.initUI()
         self.string_formatter()
         # ------THREADING-----#
-        self.thread = Worker()
-        self.connect(self.thread, SIGNAL('Hello'), self.information)
-        self.thread.start()
+        """
+        This is used to process the joystick data in the background
+        """
+        self.thread = Worker()  #Define a thread object
+        self.connect(self.thread, SIGNAL('Hello'), self.information) # Connect the incoming signal from the
+                                                                     # thread to the 'information' function
+        self.thread.start() #Start the thread
 
-        #------UDP Connection-----#
-        #recieve_socket.settimeout(1)
-        #while (True):
-        #    send_scoket.sendto(("UDP_auth".encode()), (HOST, send_port))
-        #    print('sending...')
-        #    try:
-        #        if (recieve_socket.recv(1024).decode() == 'auth_acknowledged'):
-        #            break
-        #    except:
-        #        pass
-                # sleep(5)
-        #print('Connected!')
-        recieve_socket.setblocking(0)
-
+        recieve_socket.setblocking(0) #Stop the socket from blocking the code while awaiting data
+                                      #In other words: set timeout to 0
     def initUI(self):
 
-        title1_font = QFont("Arial", 16, QFont.Bold)
-        #title2_font = QFont("Arial", 10, QFont.setUnderline(True))
+        #================ Definitions ========================
+        title1_font = QFont("Arial", 16, QFont.Bold)    #Define title1 font
 
-        application_title = QLabel()                        #Create label
+        # Title
+        application_title = QLabel()                        #Create label for the window title
         application_title.setText("ROV Control Interface")  #Set Text
-        application_title.setFont(title1_font)
+        application_title.setFont(title1_font)              #Set font
         application_title.setAlignment(Qt.AlignCenter)      #Set Allignment
 
-        self.LEDs_label = QLabel()
-        self.LEDs_label.setText("LEDs")
+        # LEDs
+        self.LEDs_label = QLabel()              #Create label for the section title
+        self.LEDs_label.setText("LEDs")         #Set Text
 
-        self.led1_label = QLabel()
-        self.led1_label.setText("Spectrum")
-        self.led2_label = QLabel()
-        self.led2_label.setText("Lights")
+        self.led1_label = QLabel()              #Create label for LED1
+        self.led1_label.setText("Spectrum")     #Set Text
+        self.led2_label = QLabel()              #Create label for LED2
+        self.led2_label.setText("Lights")       #Set Text
 
-        self.led1_indicator = QLabel()
-        self.led2_indicator = QLabel()
-        self.red_circle_indicator = QPixmap('red_circle.png')
-        self.green_circle_indicator = QPixmap('green_circle.png')
+        self.led1_indicator = QLabel()          #Create label for LED1 indicator
+        self.led2_indicator = QLabel()          #Create label for LED2 indicator
+        self.red_circle_indicator = QPixmap('red_circle.png')       #Use an image of a red circle to indicate that the LEDs are off
+        self.green_circle_indicator = QPixmap('green_circle.png')   #Use an image of a green circle to indicate that the LEDs are on
         self.led1_indicator.setPixmap(self.red_circle_indicator)
         self.led2_indicator.setPixmap(self.red_circle_indicator)
 
-        self.recieved_string_label = QLabel()
-        self.recieved_string_label.setText("String Recieved from ROV")
-        self.recieved_string_txtbox = QTextEdit()
-        self.recieved_string_txtbox.setReadOnly(True)
+        self.recieved_string_label = QLabel()   #Create label for the text received from the ROV
+        self.recieved_string_label.setText("String Recieved from ROV")  #Set Text
+        self.recieved_string_txtbox = QTextEdit()   #Create a text box to store the data received from the ROV
+        self.recieved_string_txtbox.setReadOnly(True)   #Set the text box to read only
         self.complete_recieved_string = ''
 
-        self.user_input = QTextEdit()
+        self.user_input = QTextEdit()   #Create an empty text box for the pilot to write any notes
 
-        vbox = QVBoxLayout() #Create layout container
-        vbox.addWidget(application_title)
+
+        #================ Layout ========================
+        vbox = QVBoxLayout()                #Create layout container
+        vbox.addWidget(application_title)   #Populate the container
 
         vbox.addWidget(self.LEDs_label)
 
-        LEDs_hbox = QHBoxLayout()
-        LEDs_hbox.addWidget(self.led1_label)
+        LEDs_hbox = QHBoxLayout()           #Create layout container
+        LEDs_hbox.addWidget(self.led1_label)#Populate the container
         LEDs_hbox.addWidget(self.led1_indicator)
         LEDs_hbox.addWidget(self.led2_label)
         LEDs_hbox.addWidget(self.led2_indicator)
@@ -123,16 +122,14 @@ class Window(QWidget):
         vbox.addLayout(LEDs_hbox)
 
         vbox.addWidget(self.recieved_string_label)
-        #vbox.addWidget(self.recieved_string_txtbox)
-
-        #vbox.addWidget(self.user_input)
-        recieved_string_box = QHBoxLayout()
-        recieved_string_box.addWidget(self.recieved_string_txtbox)
+        
+        recieved_string_box = QHBoxLayout() #Create layout container
+        recieved_string_box.addWidget(self.recieved_string_txtbox)  #Populate the container
         recieved_string_box.addWidget(self.user_input)
 
         vbox.addLayout(recieved_string_box)
 
-        self.setLayout(vbox)
+        self.setLayout(vbox)    #Set the layout
         
         self.setGeometry(300, 300, 300, 150)
         self.setWindowTitle('Buttons')    
@@ -140,6 +137,34 @@ class Window(QWidget):
 
     #------------What is to follow should be moved into a seprate file----------------------------
     def string_formatter(self):
+        """
+        This function formats the string that will be sent to the ROV containg the
+        commands.
+
+        The format of the string is: [FL, FU, FR, BR, BU, BL, ARM, FUN, LED1, LED2, BT]
+
+        FL: Forward Left Thruster 
+        FU: Forward Up Thruster
+        FR: Forward Right Thruster
+        BR: Backward Right Thruster
+        BU: Backward Up Thruster
+        BL: Backward Left Thruster
+        Thrusters values are between 1100 and 1900, with 1500 being nominal (at rest).
+
+        ARM: Manipulator Arm
+        FUN: Funnel
+        0 -> not moving
+        1 -> clockwise/opening
+        2 -> anti-clockwise/closing
+
+        LED1: On-baord LED
+        LED2: On-baord LED
+        0 -> OFF
+        1 -> ON
+
+
+        BT: Bluetooth
+        """
         # ------ Storing the values from the different axis on joystick------#
         self.X_Axis = my_joystick.get_axis(0)  # X_Axis- Axis 0
         self.Y_Axis = my_joystick.get_axis(1)  # Y_Axis - Axis 1
@@ -152,34 +177,48 @@ class Window(QWidget):
         self.arm_close_button = my_joystick.get_button(7)  # Button 8
         self.LED1_button = my_joystick.get_button(10)  # Button SE
         self.LED2_button = my_joystick.get_button(11)  # Button ST
+
+        # Bluetooth controls
         self.BT_button1 = my_joystick.get_button(0)
         self.BT_button2 = my_joystick.get_button(1)
+        
+        # Initital values
         self.BT = 0
-
-        if(self.BT_button1 == 1):
-            self.BT = 1
-        elif(self.BT_button2 == 1):
-            self.BT = 2
-
-        # self.Roll= my_joystick.get_button(0)
-
         self.funnel = 0
         self.arm = 0
         global LED1
         global LED2
 
-        # ------ Thrusters Power
+        # ================================ Thrusters Power ================================
+        """
+        Power: Overall scaling factor (0.4 = 40% of the full power)
+        Fwd_factor: The power factor when moving forward/backward
+        Side_factor: The power factor when turning around (yaw)
+
+        The values for the four thrusters in the horizontal plane (fwd_left_thruster,
+        fwd_right_thruster, bck_left_thruster and bck_right_thruster) is calculated by
+        taking in the value of each of the joystic three main control axis (X, Y and Yaw)
+        values and multplying each value by the appropriate scaling factor. These values
+        are then added with respect to the relevant sign (+ve/-ve).
+
+        This is a reasonable approximation; however, this calculation has a limited
+        domain at which it is valid. Specfically, at each of the four joystick diagonal
+        axis (i.e: when X AND Y are equal to 1 or -1) the resultant value goes beyond the
+        boundaries (1100 and 1900). Therefore, a condition has been written to handle this
+        problem by dividing the resultant value by 2 (e.g: 3800 becomes 1900). This is not
+        a perfect scenario but it is acceptable for this application.
+        """
         self.power = 0.4
         self.fwd_factor = 400 * self.power
         self.side_factor = 400 * self.power
-        self.yaw_factor = 200  # minimum drag
+        self.yaw_factor = 200
 
         # Account for double power in case of diagonals
         if ((self.X_Axis > 0.1 and self.Y_Axis < -0.1) or
                 (self.X_Axis < -0.1 and self.Y_Axis > 0.1) or
                 (self.X_Axis < -0.1 and self.Y_Axis < -0.1) or
                 (self.X_Axis > 0.1 and self.Y_Axis > 0.1)):
-            self.fwd_factor = 200 * self.power
+            self.fwd_factor = 200 * self.power      # multiply by half of the power factor
             self.side_factor = 200 * self.power
 
         self.fwd_left_thruster = int(
@@ -191,26 +230,35 @@ class Window(QWidget):
         self.bck_right_thruster = int(
             1500 + self.fwd_factor * self.Y_Axis - self.side_factor * self.X_Axis + self.yaw_factor * self.Yaw)
 
+
+        # To go up/down
         self.front_thruster = int(1500 + self.fwd_factor * self.Rudder)
         self.back_thruster = int(1500 + self.fwd_factor * self.Rudder)
 
         # ------Pitching code------
+        """
+        To pitch up/down the pilot needs to put the throttle in the +ve or -ve position. This overides
+        the above 2 lines and moves the thrusters in oppsite directions in order to pitch as required.
+        """
         if(self.Throttle>0.1 or self.Throttle<-0.1):
             self.front_thruster = int(1500 - self.fwd_factor * self.Throttle)
             self.back_thruster = int(1500 - self.fwd_factor * self.Throttle)
-        # -------------------------
 
 
+        # ================================ Manipulators ================================
+        # Funnel
         if (self.funnel_CW_button == 1):
             self.funnel = 1
         elif (self.funnel_CCW_button == 1):
             self.funnel = 2
 
+        # Arm
         if (self.arm_open_button == 1):
             self.arm = 1
         elif (self.arm_close_button == 1):
             self.arm = 2
 
+        # LED1
         if (self.LED1_button == 1):
             sleep(0.2)
             if (LED1 == 1):
@@ -220,6 +268,7 @@ class Window(QWidget):
                 LED1 = 1
                 self.led1_indicator.setPixmap(self.green_circle_indicator)
 
+        # LED2
         if (self.LED2_button == 1):
             sleep(0.2)
             if (LED2 == 1):
@@ -229,10 +278,17 @@ class Window(QWidget):
                 LED2 = 1
                 self.led2_indicator.setPixmap(self.green_circle_indicator)
 
+        # Bluetooth
+        if(self.BT_button1 == 1):
+            self.BT = 1
+        elif(self.BT_button2 == 1):
+            self.BT = 2
+
+        # Final string to be sent
         self.stringToSend = str([self.fwd_left_thruster, self.front_thruster, self.fwd_right_thruster,
                                  self.bck_right_thruster, self.back_thruster, self.bck_left_thruster,
                                  self.arm, self.funnel, self.BT_button1, LED2, self.BT])
-        print(self.stringToSend)
+        print(self.stringToSend) # Print final string
 
     def information(self):
         # self.comma=","
@@ -240,9 +296,6 @@ class Window(QWidget):
         name_joystick = my_joystick.get_name()  # Collects the pre-defined name of joystick
         number_axes = my_joystick.get_numaxes()  # Collects the pre-defined number of axis
         number_buttons = my_joystick.get_numbuttons()  # Collects the pre-defined number of buttons
-        #self.txt1.setText(str(name_joystick))  # Displaying the information
-        #self.txt2.setText(str(number_axes))  # in the required textboxes
-        #self.txt3.setText(str(number_buttons))
         send_scoket.sendto((self.stringToSend.encode()), (HOST, send_port))  # The thing that we send
         try:
             recieved_string = recieve_socket.recv(1024).decode()
@@ -252,48 +305,6 @@ class Window(QWidget):
         except:
             pass
 
-        # send_scoket.sendto(str(self.comma).encode(),(HOST,send_port))
-        # -------------------------------------#
-
-        # ---------Collecting the value of the Axis---------#
-        #self.txt4.setText(str("{:>.2f}".format(my_joystick.get_axis(0))))
-        #   send_scoket.sendto(str("{:>.2f}".format(my_joystick.get_axis(0))).encode(),(HOST,send_port))
-        #self.txt5.setText(str("{:>.2f}".format(my_joystick.get_axis(1))))
-        #  send_scoket.sendto(str("{:>.2f}".format(my_joystick.get_axis(1))).encode(),(HOST,send_port))
-        #self.txt6.setText(str("{:>.2f}".format(my_joystick.get_axis(2))))
-        # send_scoket.sendto(str("{:>.2f}".format(my_joystick.get_axis(2))).encode(),(HOST,send_port))
-        #self.txt7.setText(str("{:>.2f}".format(my_joystick.get_axis(3))))
-        # send_scoket.sendto(str("{:>.2f}".format(my_joystick.get_axis(3))).encode(),(HOST,send_port))
-        #self.txt8.setText(str("{:>.2f}".format(my_joystick.get_axis(4))))
-        # send_scoket.sendto(str("{:>.2f}".format(my_joystick.get_axis(4))).encode(),(HOST,send_port))
-        # ---------------------------------------------------#
-
-        # ------------------Collecting the value of Buttons--------------#
-        #self.txt9.setText(str(my_joystick.get_button(0)))
-        # send_scoket.sendto(str(my_joystick.get_button(0)).encode(),(HOST,send_port))
-        #self.txt10.setText(str(my_joystick.get_button(1)))
-        # send_scoket.sendto(str(my_joystick.get_button(1)).encode(),(HOST,send_port))
-        #self.txt11.setText(str(my_joystick.get_button(2)))
-        # send_scoket.sendto(str(my_joystick.get_button(2)).encode(),(HOST,send_port))
-        #self.txt12.setText(str(my_joystick.get_button(3)))
-        # send_scoket.sendto(str(my_joystick.get_button(3)).encode(),(HOST,send_port))
-        #self.txt13.setText(str(my_joystick.get_button(4)))
-        # send_scoket.sendto(str(my_joystick.get_button(4)).encode(),(HOST,send_port))
-        #self.txt14.setText(str(my_joystick.get_button(5)))
-        # send_scoket.sendto(str(my_joystick.get_button(5)).encode(),(HOST,send_port))
-        #self.txt15.setText(str(my_joystick.get_button(6)))
-        # send_scoket.sendto(str(my_joystick.get_button(6)).encode(),(HOST,send_port))
-        #self.txt16.setText(str(my_joystick.get_button(7)))
-        # send_scoket.sendto(str(my_joystick.get_button(7)).encode(),(HOST,send_port))
-        #self.txt17.setText(str(my_joystick.get_button(8)))
-        # send_scoket.sendto(str(my_joystick.get_button(8)).encode(),(HOST,send_port))
-        #self.txt18.setText(str(my_joystick.get_button(9)))
-        # send_scoket.sendto(str(my_joystick.get_button(9)).encode(),(HOST,send_port))
-        #self.txt19.setText(str(my_joystick.get_button(10)))
-        # send_scoket.sendto(str(my_joystick.get_button(10)).encode(),(HOST,send_port))
-        #self.txt20.setText(str(my_joystick.get_button(11)))
-        # send_scoket.sendto(str(my_joystick.get_button(11)).encode(),(HOST,send_port))
-        # ----------------------------------------------------------------
         self.string_formatter()  # Calling the thruster value
 
 
